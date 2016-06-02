52f5e4d1d81e333b02df0a3af2f97ab1533818ca
Varek Boettcher <varekb@gmail.com>
2016-06-02 14:22:56 -0700

    Initial commit

---
+++ src/main/java/ca/bc/gov/proto_service/ProtoServiceApp.java
@@ -0,0 +1,17 @@
+package ca.bc.gov.proto_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+public class ProtoServiceApp 
+{
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoServiceApp.class, args);
+	}
+}
---
+++ src/main/java/ca/bc/gov/proto_service/services/ProtoServiceResource.java
@@ -0,0 +1,21 @@
+package ca.bc.gov.proto_service.services;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/protoservice")
+public class ProtoServiceResource {
+	
+	/**
+	 * My cool new prototype webservice to deploy to openshift
+	 */
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String helloWorld() {
+		final String answer="Shiny!";
+		return answer;
+	}
+	
+}
